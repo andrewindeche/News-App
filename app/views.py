4e56997785359f99518db1ef9afe8c000b26ee0d
AndrewIndeche <indecheandrew@gmail.com>
2021-09-11 11:14:57 +0300

    Add dynamic routes for article file

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render template
+from flask import render_template
 from app import app
 
 @app.route('/')
@@ -7,4 +7,12 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    message='This is your favorite News Application'
+    return render_template('index.html',message = message)
+
+@app.route('/article/<article_id>')
+def article(article_id)
+     '''
+    View arrticle page function that returns the movie details page and its data
+    '''
+    return render_template('article.html',id = article_id)
