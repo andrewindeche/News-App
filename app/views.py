6daa3cd547cca2b92441209ac0d3af3da756f792
AndrewIndeche <indecheandrew@gmail.com>
2021-09-11 12:31:03 +0300

    Add configuration class and initialize configuration

--- app/views.py
+++ app/views.py
@@ -3,16 +3,16 @@ from app import app
 
 @app.route('/')
 def index():
-
     '''
     View root page function that returns the index page and its data
     '''
     message='This is your favorite News Application'
     return render_template('index.html',message = message)
 
-@app.route('/article/<article_id>')
-def article(article_id)
+@app.route('/article/<int:article_id>')
+def article(article_id):
+
      '''
-    View arrticle page function that returns the movie details page and its data
-    '''
-    return render_template('article.html',id = article_id)
+     View arrticle page function that returns the movie details page and its data
+     '''
+     return render_template('article.html',id = article_id)
--- run.py
+++ run.py
@@ -1,4 +1,11 @@
 from app import app
+from .config import DevConfig
+
+# Initializing application
+app = Flask(__name__)
+
+# Setting up configuration
+app.config.from_object(DevConfig)
 
 if __name__ == '__main__':
     app.run(debug = True)
