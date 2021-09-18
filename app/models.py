6ecdc28e910bf80bb0f0331002dee0c00ad4bd2f
AndrewIndeche <indecheandrew@gmail.com>
2021-09-18 09:50:47 +0300

    correction of image sizes

--- app/models.py
+++ app/models.py
@@ -12,12 +12,11 @@ class Headlines:
     '''
     Article Class that instantiates objects of the headlines categories objects of the news sources
     '''
-    def __init__(self,author,description,time,url,url_to_imageImage,image,title):
+    def __init__(self,author,description,time,url,image,title):
         self.author = author
         self.description = description
         self.time= time
         self.url = url
-        self.url_to_image= url_to_image
         self.image = image
         self.title = title
 
